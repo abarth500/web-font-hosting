acd53f95fc850006e5fe07553e85207416c1ecb1
Shohei Yokoyama <shohei.yokoyama@gmail.com>
2013-05-20 13:15:06 +0900

    bel-bambino

---
+++ bel-bambino.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	var name="横山 りよ";
+	var account="belbambino";
+	var domain="hotmail";
+	var tld="jp"
+	$(".belB-contact").text('<div style="width:100px;float:left;"><img src="http://img01.hamazo.tv/usr/b/e/l/belbambino/qr-mail.png"></div><div style="text-align:center;width:300px;float:left;">ベビーマッサージサークル <b>ベルバンビーノ</b><br><span style="font-size:200%;line-height:150%;">*＊<span style="font-family: xiomara;">Bel Bambino!</span>＊*</span><br><span style="font-size:150%">代表：<b>横山 りよ</b></span><br><a href="mailto:'+account+'@'+domain+'.'+tld+'">'+account+'@'+domain+'.'+tld+'</a></div><br clear="both">');
+});
